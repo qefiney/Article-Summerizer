b876fc415a6c5a63b0f324a1131e69b535a1911a
qefiney <qefiney@gmail.com>
2023-09-15 19:07:23 +0300

    Added hero section content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { logo } from "../assets";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+	return (
+		<>
+			<header className="w-full justify-center items-center flex-col">
+				<nav className="flex justify-between items-center w-full mb-10 pt-3">
+					<img src={logo} alt="app logo" className="w-28 object-contain" />
 
-export default Hero
+					<button
+						type="button"
+						onClick={() =>
+							window.open("https://github.com/qefiney/Article-Summerizer")
+						}
+						className="black_btn">
+						Github
+					</button>
+				</nav>
+
+				<h1 className="head_text">
+					Summarize Article with <br className="max-md:hidden" />
+					<span className="orange_gradient">OpenAI GPT-4</span>
+				</h1>
+
+				<h2 className="desc">
+					Make your reading short and simple with Sumz, an open-source article
+					summarizer that transforms lengthy article into clear and concise
+					summaries.
+				</h2>
+			</header>
+		</>
+	);
+};
+
+export default Hero;
